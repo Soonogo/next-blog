9431fa6f27d8636a5a4a70ccd3b5ba99ada0dd86
songen <614750803@qq.com>
2022-09-14 23:36:45 +0800

    添加三个表

--- README.md
+++ README.md
@@ -36,6 +36,10 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/deploym
 ```shell
 docker run -v "$PWD/blog-data":/var/lib/postgresql/data -p 5432:5432 -e POSTGRES_USER=blog -e POSTGRES_HOST_AUTH_METHOD=trust -d postgres:12.2
 ```
-`CREATE DATABASE blog_development ENCODING 'UTF8' LC_COLLATE 'en_US.utf8' LC_CTYPE 'en_US.utf8';`
+```
+CREATE DATABASE blog_development ENCODING 'UTF8' LC_COLLATE 'en_US.utf8' LC_CTYPE 'en_US.utf8';
+```
 
-`npx babel ./src --out-dir dist --extensions ".ts,.tsx"`
+```
+npx babel ./src --out-dir dist --extensions ".ts,.tsx"
+```
--- src/migration/1663167793398-CreatePosts.ts
+++ src/migration/1663167793398-CreatePosts.ts
@@ -1,20 +1,21 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export class CreateUsers1663166681909 implements MigrationInterface {
+export class CreatePosts1663167793398 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-      return await  queryRunner.createTable(new Table({
-            name: 'users',
+        return await  queryRunner.createTable(new Table({
+            name: 'posts',
             columns: [
                 {name: 'id', isGenerated: true, type: 'int', generationStrategy: 'increment', isPrimary: true},
-                {name: 'username', type: 'varchar'},
-                {name: 'password_digest', type: 'varchar'},
+                {name: 'title', type: 'varchar'},
+                {name: 'content', type: 'varchar'},
+                {name: 'author_id', type: 'int'},
             ]
         }));
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-      return await queryRunner.dropTable('users');
+       return await queryRunner.dropTable('posts')
     }
 
 }
